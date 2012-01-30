60921446cc6f2dbaf22a2fa50a7ef9f20ed60476
Kortina <kortina@gmail.com>
2012-01-30 16:49:49 -0500

    added reverse tunnel instructions

--- bash-cheatsheet.sh
+++ bash-cheatsheet.sh
@@ -41,3 +41,31 @@ test -h /path/possible_sym && echo "possible_sym IS a symlink"
 
 # file renaming util
 for i in *.avi; do j=`echo $i | sed 's/find/replace/g'`; mv "$i" "$j"; done
+
+
+# ssh tunnel
+# http://blog.kenweiner.com/2007/09/reverse-ssh-tunnel-for-facebook.html
+
+ssh -gNR 8888:localhost:8080 www.myremoteserver.com
+
+"Forward requests on www.myremoteserver.com:8888 to my workstation port 8080."
+
+     -g Allows remote hosts to connect to local forwarded ports.
+     -N Do not execute a remote command.  This is useful for just forwarding ports (protocol version 2 only).
+     -R [bind_address:]port:host:hostport
+        Specifies that the given port on the remote (server) host is to be
+        forwarded to the given host and port on the local side.  This works by
+        allocating a socket to listen to port on the remote side, and whenever
+        a connection is made to this port, the connection is forwarded over the
+        secure channel, and a connection is made to host port hostport from the
+        local machine.
+
+
+NB: You will need to edit /etc/ssh/sshd_config and add the following line:
+
+    GatewayPorts yes
+
+Forward requests made to localhost:27018 to remoteserver:27017
+
+    ssh -N -i ./rsa_private_key -L 27018:localhost:27017 ubuntu@remoteserver.com
+
