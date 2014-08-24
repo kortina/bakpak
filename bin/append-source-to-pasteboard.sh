661254ff05f772f31d703910d7ab08b163edf465
Kortina <kortina@gmail.com>
2014-08-24 19:25:57 -0400

    only url

--- bin/append-source-to-pasteboard.sh
+++ bin/append-source-to-pasteboard.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 URL=`osascript -e "tell application \"Google Chrome\" to return URL of active tab of front window"`
-TITLE=`osascript -e "tell application \"Google Chrome\" to return title of active tab of front window"`
+# TITLE=`osascript -e "tell application \"Google Chrome\" to return title of active tab of front window"`
 PBP=`pbpaste`
-echo -e "$PBP\n\n-- via: [$TITLE]($URL)" | pbcopy
+# echo -e "$PBP\n\n-- via: [$TITLE]($URL)" | pbcopy
+echo -e "$PBP\n\n-- via: $URL" | pbcopy
