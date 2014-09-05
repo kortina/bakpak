cf6769a3523cbe5197b99c27b1c0b6206940f342
Kortina <kortina@gmail.com>
2014-09-05 16:04:28 -0400

    make instapaper import a bit more UNIX-y

--- bin/instapaper-import.sh
+++ bin/instapaper-import.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # 
-# put a list of links you would like to save in a file named
-# urls.txt
-#
-# run bash instapaper-import.sh from directory containing urls.txt
+# USAGE
+# ./instapaper-import.sh http://www.multivax.com/last_question.html
+# 
+# BULK
+# put a list of urls in a file named `urls.txt`
+# and run 
+# cat urls.txt | xargs -I {} ./instapaper-import.sh "{}"
 
 
 if [[ -z "$INSTAPAPER_EMAIL" ]]; then
-    echo "must set \$INSTAPAPER_EMAIL shell var, eg"
+    echo "You must set \$INSTAPAPER_EMAIL shell var, eg"
     echo "export INSTAPAPER_EMAIL=\"your-secret-email@instapaper.com\""
+    echo -e "\nGet your secret import email here: \nhttps://www.instapaper.com/save/email"
     exit 1
 fi
-if [ ! -f urls.txt ]; then
-    echo "file urls.txt does not exist in cwd"
+if [[ -z "$1" ]]; then
+    echo "Supply a url as first and only arg"
+    echo "eg, ./instapaper-import.sh \"http://blog.instapaper.com/\""
     exit 1
 fi
 
-echo "Sending email to: $INSTAPAPER_EMAIL"
-for url in `cat urls.txt`; do 
-    echo "$url" > /tmp/u && mail -s "" $INSTAPAPER_EMAIL < /tmp/u ;
-done
-
-echo "emailed urls in urls.txt"
+echo "Sending $1 via email to: $INSTAPAPER_EMAIL"
+echo "$1" > /tmp/u && mail -s "" $INSTAPAPER_EMAIL < /tmp/u ;
