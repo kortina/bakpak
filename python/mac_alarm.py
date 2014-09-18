0aad284d35da2a8802d1f191e26d755953a3a70b
Kortina <kortina@gmail.com>
2014-09-18 18:12:10 -0400

    print countdown

--- python/mac_alarm.py
+++ python/mac_alarm.py
@@ -10,10 +10,14 @@ def alarm(seconds=0, minutes=0):
     print "Alarm will sound in {0} min {1} sec".format(minutes, seconds)
     print "End by pressing Ctrl+C"
     seconds += minutes * 60
-    time.sleep(seconds)
-    while 1:
+    while seconds > 0:
+        print "Wake in {0} sec".format(seconds)
         time.sleep(1)
+        seconds -= 1
+
+    while 1:
         subprocess.call(["say", "a"])
+        time.sleep(1)
 
 if __name__ == "__main__":
     args = parser.parse_args()
