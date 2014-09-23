2d0d7435e4690d7694af07bb9ea1d91c8b714b9c
Kortina <kortina@gmail.com>
2014-09-23 19:04:10 -0400

    modify from n2s

--- bin/note-to-self.sh
+++ bin/note-to-self.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-FROM="kortina@gmail.com"
+FROM="kortina+bash@gmail.com"
 TO="kortina+notes@gmail.com"
 SUBJECT=$(cat) # read from STDIN
 echo -e "Subject: $SUBJECT\n\n" | sendmail -f "$FROM" "$TO"
