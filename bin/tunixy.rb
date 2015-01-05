bb71065cc605c331cf7135e39a9836e2f11b9f29
Kortina <kortina@gmail.com>
2015-01-05 10:54:48 -0500

    add unixy text util

---
+++ bin/tunixy.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+usage = <<EOF
+Make some text more "unixy"
+
+tunixy.rb
+
+Example:
+echo -e "“" | tunixy.rb 
+"
+
+EOF
+=begin
+Installation:
+
+chmod 700 tunixy.rb
+mv tunixy.rb to a directory on your PATH
+
+=end
+
+# replace double curly quotes
+# replace curly single quotes
+# replace tabs with newlines
+# replace 3+ newlines with only 2 newlines
+puts STDIN.read.gsub(/“|”/, '"').gsub(/‘|’/, "'").gsub(/\r/, "\n").gsub(/^\t+/, "\n").gsub(/\n{3,}/, "\n\n")
