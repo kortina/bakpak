06f536082d0ceaa1bae3e768fd7a702dce965caf
Kortina <kortina@gmail.com>
2015-01-09 10:48:28 -0500

    minor updates

--- bin/tunixy.rb
+++ bin/tunixy.rb
@@ -20,6 +20,7 @@ mv tunixy.rb to a directory on your PATH
 
 # replace double curly quotes
 # replace curly single quotes
+# replace control characters
 # replace tabs with newlines
 # replace 3+ newlines with only 2 newlines
-puts STDIN.read.gsub(/“|”/, '"').gsub(/‘|’/, "'").gsub(/\r/, "\n").gsub(/^\t+/, "\n").gsub(/\n{3,}/, "\n\n")
+puts STDIN.read.gsub(/“|”/, '"').gsub(/‘|’/, "'").gsub(/\^L+/, "").gsub(/\r/, "\n").gsub(/^\t+/, "\n").gsub(/\n{3,}/, "\n\n")
